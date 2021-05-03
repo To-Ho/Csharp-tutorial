685b29a54378aff997e1d606102c7aacb4be6f92
To-Ho <83618379+To-Ho@users.noreply.github.com>
2021-05-03 17:53:46 -0600

    Add files via upload

---
+++ Interface calculate/Interface calculate/Program.cs
@@ -0,0 +1,52 @@
+﻿//Geometric calculate
+//Perimeter, Area 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_calculate
+{
+    //Declare a shape interface
+    interface IShape
+    {
+        void perimeterCal(double a, double b);
+        void areaCal(double a, double b);
+    }
+
+    //Class retangular
+    class Retangular : IShape
+    {
+        public void perimeterCal(double a,double b)
+        {
+            double Peri = (a + b) * 2;
+            Console.WriteLine("Retangular perimeter is: " + Peri);
+        }
+
+        public void areaCal(double a, double b)
+        {
+            double Area = (a * b);
+            Console.WriteLine("Retangular area is: " + Area);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3;
+            double y = 5;
+
+            Console.WriteLine("x = {0}, y = {1}", x, y);
+            //Declare shape object
+            Retangular myRetangular = new Retangular();
+
+
+            //Calculation
+            myRetangular.perimeterCal(x, y);
+            myRetangular.areaCal(x, y);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InterfaceExamples/InterfaceExamples/Program.cs
@@ -0,0 +1,62 @@
+﻿//Examples using interface
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExamples
+{
+    //Declare interface
+    interface IAnimal
+    {
+        void animalISound();
+        void animalIFood();
+    }
+
+    //Pig inplements the IAnimal interface
+    class Pig : IAnimal
+    {
+        public void animalISound()
+        {
+            Console.WriteLine("oi oi");
+        }
+
+        public void animalIFood()
+        {
+            Console.WriteLine("Anything");
+        }
+    }
+
+    //Horse class for IAnimal interface
+    class Horse : IAnimal
+    {
+        public void animalISound()
+        {
+            Console.WriteLine("Neight!");
+        }
+        public void animalIFood()
+        {
+            Console.WriteLine("Grass, grain, and carrot");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig(); //Create a pig object
+            Horse myHorse = new Horse(); //Create a horse object
+            Console.Write("Pig sound:");
+            myPig.animalISound();
+            Console.Write("Pig eats: ");
+            myPig.animalIFood();
+
+            Console.Write("Horse sound: ");
+            myHorse.animalISound();
+            Console.Write("Horse eat: ");
+            myHorse.animalIFood();
+
+            Console.ReadKey();
+        }
+    }
+}
